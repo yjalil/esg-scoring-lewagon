913bb45bbc073d61b7155b7c9d4db0c6cb9850a3
yjalil <jalil.yahy@gmail.com>
2023-01-20 09:43:26 +0100

    common-preprocessing-class-sandbox.ipynb

--- notebooks/yjalil/utils.py
+++ notebooks/yjalil/utils.py
@@ -1,6 +1,67 @@
-import pandas as pd
+import string
+from unidecode import unidecode
+from sklearn.base import BaseEstimator,TransformerMixin
+from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
+from nltk.corpus import stopwords
+from nltk.stem import WordNetLemmatizer, PorterStemmer
+from nltk.tokenize import word_tokenize
 
-def combine_labels(df):
-    df_copy = df.copy()
-    df_copy['combined_label'] = df_copy['topic_label'] + df_copy['sentiment_label']
-    return df_copy
+class CustomPreprocessor(BaseEstimator,TransformerMixin):
+    ''' Preprocessor with multiple options for gridSearchCv
+    - remove_stopwords : {True,False}. Can keep stopwords if set to false. Default=True.
+    - negation: {'keep','remove'}. Choose to keep or remove (no,not) from text. Default='keep'.
+    - numbers: {'keep','remove'}. Choose to keep or remove numbers from the text. Default='keep'.
+    - punctuation: {'keep','remove'}. Choose to keep or remove punctuation from the text. Default='remove'.
+    - accents: {'keep','remove'}. Choose to keep or remove accents from the text. Default='keep'.
+    - html: {'keep','remove'}. Choose to keep or remove html tags from the text. Default='keep'.
+    - lemma: {True, False}. Choose to lemmatize or not. Default=False.
+    - stem: {True, False}. Choose to stem or not. Default=False.
+    '''
+    def __init__(self,remove_stopwords=True,negation = 'keep',numbers='keep',punctuation='remove',accents='keep', html='keep',lemma=False, stem=False):
+        
+        self.negation_list = ['no','not']
+        self.remove_stopwords = remove_stopwords
+        self.numbers = numbers
+        self.html = html
+        self.accents = accents
+        self.lemma = lemma
+        self.stem = stem
+        self.punctuation = punctuation
+        self.negation = negation
+        if self.negation == 'keep' :
+            self.stop_words = [word for word in stopwords.words('english') if word not in self.negation_list]
+        if self.negation == 'remove' :
+            self.stop_words ==  stopwords.words('english')
+        
+        
+    def fit(self, X, y=None):
+        return self
+
+    def transform(self, X, y=None):
+        
+        if self.numbers == 'remove':
+            X['sentence'] = X['sentence'].str.replace(r'\d+', '', regex=True)
+           
+        if self.punctuation == 'remove':
+            X['sentence'] = X['sentence'].apply(lambda x: x.translate(str.maketrans('', '', string.punctuation)))
+            
+        if self.html == 'remove':
+            X['sentence'] = X['sentence'].str.replace(r'<.*?>|&([a-z0-9]+|#[0-9]{1,6}|#x[0-9a-f]{1,6});','', regex=True)
+           
+        if self.lemma :
+            self.lemmatizer = WordNetLemmatizer()
+            X['sentence'] = X['sentence'].apply(lambda  x: ' '.join([self.lemmatizer.lemmatize(word) for word in x.split()]))
+           
+        if self.stem :
+            self.stemmer = PorterStemmer()
+            X['sentence'] = X['sentence'].apply(lambda x: ' '.join([self.stemmer.stem(word) for word in x.split()]))    
+              
+        
+        if self.remove_stopwords:
+            X['sentence'] = X['sentence'].apply(lambda x: ' '.join([word for word in word_tokenize(x) if word.lower() not in self.stop_words]))
+            
+
+        if self.accents == 'remove':
+            X['sentence'] = X['sentence'].apply(lambda x: unidecode(x))
+            
+        return X['sentence'].values
